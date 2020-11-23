e0bb6d15de53e2ee35e6ea152ba67e8536c6d2c0
Lucette Das <l.k.das@st.hanze.nl>
2020-11-23 16:51:20 +0100

    Je kan nu gebruikers uit de Database halen

--- src/main/java/nl/miwgroningen/cohort4/lucette/libraryDemo/configuration/LibrarySecurityConfiguration.java
+++ src/main/java/nl/miwgroningen/cohort4/lucette/libraryDemo/configuration/LibrarySecurityConfiguration.java
@@ -1,10 +1,12 @@
 package nl.miwgroningen.cohort4.lucette.libraryDemo.configuration;
 
+import nl.miwgroningen.cohort4.lucette.libraryDemo.services.LibraryUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,10 +19,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class LibrarySecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    LibraryUserDetailsService libraryUserDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin")).roles("USER");
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(libraryUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
     @Bean
